a0d4f6943edce0f93ecffabea7efba7e0c519297
LeChristopher Blackwell <codeblackwell@gmail.comgit status\curl -sSL https://get.rvm.io | bash -s stablegit config --global user.email codeblackwell@gmail.com>
2016-03-03 17:30:33 -0800

    init commit

---
+++ server/server.js
@@ -0,0 +1,44 @@
+var express = require("express");
+var path = require('path');
+var bodyParser = require('body-parser');
+var webpack = require('webpack');
+var WebpackDevServer = require('webpack-dev-server');
+var config = require('../webpack.config.js');
+var mongoose = require('mongoose');
+
+var app = express();
+
+
+var databaseCollection = require('../data/db/MongooseSchema.model');
+
+//Database Names have an 's' added
+var db = 'mongodb://localhost/databaseName';
+
+mongoose.connect(db);
+
+var port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/../'));
+
+app.listen(port, function () {
+ console.log('Proxy listening on port 3000!');
+});
+
+
+
+new WebpackDevServer(webpack(config), {
+ hot: true,
+ historyApiFallback: true,
+ proxy: {
+   "*": "http://localhost:3000"
+ }
+}).listen(3001, 'localhost', function (err, result) {
+ if (err) {
+   console.log(err);
+ }
+  console.log('Listening at localhost:3001');
+});
+
+
+
+/////////////////////////////
